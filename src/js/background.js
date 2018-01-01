5ac242e0da1a599bd36a221a3553c197d0d7de97
Renan Gomes Barreto <email@renangomes.com>
2018-01-01 16:20:00 -0300

    Version 0.0.1

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,9 @@
-import handlers from './modules/handlers';
 import msg from './modules/msg';
+
+
+import lists from './lists.js';
+
+console.log(lists.all);
 
 // here we use SHARED message handlers, so all the contexts support the same
 // commands. in background, we extend the handlers with two special
@@ -12,29 +16,167 @@ import msg from './modules/msg';
 // know when new instance of given context is created / destroyed, or you want
 // to be able to issue command requests from this context), you may simply
 // omit the `hadnlers` parameter for good when invoking msg.init()
+console.log('BACKGROUND SCRIPT RUNNING'); // eslint-disable-line no-console
 
-console.log('BACKGROUND SCRIPT WORKS!'); // eslint-disable-line no-console
-
-// adding special background notification handlers onConnect / onDisconnect
+/**
+ * Special background notification handlers onConnect / onDisconnect
+ * @param {*} ev Enviroment
+ * @param {*} context  The contect
+ * @param {*} tabId  The tab id
+ */
 function logEvent(ev, context, tabId) {
   console.log(`${ev}: context = ${context}, tabId = ${tabId}`); // eslint-disable-line no-console
 }
-handlers.onConnect = logEvent.bind(null, 'onConnect');
-handlers.onDisconnect = logEvent.bind(null, 'onDisconnect');
-const message = msg.init('bg', handlers.create('bg'));
 
-// issue `echo` command in 10 seconds after invoked,
-// schedule next run in 5 minutes
-function helloWorld() {
-  console.log('===== will broadcast "hello world!" in 10 seconds'); // eslint-disable-line no-console
-  setTimeout(() => {
-    console.log('>>>>> broadcasting "hello world!" now'); // eslint-disable-line no-console
-    message.bcast('echo', 'hello world!', () =>
-      console.log('<<<<< broadcasting done') // eslint-disable-line no-console
-    );
-  }, 10 * 1000);
-  setTimeout(helloWorld, 5 * 60 * 1000);
+
+/** All domain filters */
+var filters = {};
+
+
+/**
+ * Create the HTML to replace the ad, if needed
+ * @param {*} selector  The css selector
+ * @param {*} index  The indext that will match inside that selector
+ * @param {*} width  The width
+ * @param {*} height the height
+ * @param {*} callback the callback
+ */
+function doReplace(selector, index, width, height, callback) {
+  if (width > 40 && height > 40) {
+      callback('<div style="width:'+width+'px;height:'+height+'px;background:#ff9966;display:block;"></div>');
+  }  else {
+      callback('<div style="width:'+width+'px;height:'+height+'px;background:transparent;display:block;outline:3px dashed #ff9966;outline-offset:-2px;"></div>');
+  }
 }
 
-// start broadcasting loop
-helloWorld();
+/**
+ * Discover all filters for the current domain
+ * @param string domain  The current domain name
+ * @param function domain  The callback function that will be called at the end.
+ */
+function doFilter(domain, callback) {
+
+  console.log(`Backgound function called 'dofilter' for the domain '${domain}'`);
+
+  var keysMatched = [];
+
+  if (!domain || domain.length == 0) {
+    return null;
+  }
+
+  var result = "";
+
+  var keys = Object.keys(filters)
+
+  // For each key on the filters, try to see if the current domain engs with any one the domains of the filter.
+  for (var index = 0; index < keys.length; ++index) {
+    if (keys[index] && keys[index].length > 0 && (domain.startsWith(keys[index]) || domain.endsWith("." + keys[index]) )){
+
+      keysMatched.push(keys[index]);
+
+      if (result.length != 0) {
+        result += ",";
+      }
+      result += filters[keys[index]];
+    }
+  }
+
+  console.info(`Filters for domain '${domain}' matched with keys '${keysMatched}' and the result is '${result}'`);
+
+  callback(result);
+}
+
+
+
+/**
+ * Parse the list and let it ready to be used.
+ */
+function parseList() {
+  console.log("Parsing the filters...");
+  var lines = this.responseText.match(/[^\r?\n]+/g);
+
+  for (var index = 0; index < lines.length; ++index) {
+
+    // If is a filter rule
+    if (lines[index].indexOf("##") != -1) {
+
+      //If it is a a custom filter for adblockplus, or has a negative command, skip
+      if (lines[index].indexOf("-abp") != -1 || lines[index].indexOf("~") != -1 || lines[index].indexOf("#@#") != -1) {
+        continue;
+      }
+
+      var parts = lines[index].split("##");
+      var domains = parts[0].split(',');
+      var selector = parts[1];
+
+      // for each domain (ignores the global ones)
+      if (domains && domains.length > 0) {
+        for (var dId = 0; dId < domains.length; ++dId) {
+          if (filters[domains[dId]]) {
+            filters[domains[dId]] = filters[domains[dId]] + "," + selector;
+          } else {
+            filters[domains[dId]] = selector;
+          }
+        }
+      }
+
+    }
+
+  }
+  console.log("Filters loaded:");
+  console.log(filters);
+}
+
+/**
+ * Open the filter file
+ * @param string url The url for the list
+ */
+function loadList(url) {
+  console.log("Loading filter list....")
+  var x = new XMLHttpRequest();
+  x.onload = parseList;
+  x.open("GET", url, true);
+  x.send();
+}
+
+// Start loading the list of filters based on the local file
+//loadList(chrome.runtime.getURL("/filters.txt"));
+//loadList(chrome.runtime.getURL("/filters.txt"));
+
+for (var count = 0; count < lists.all.length; ++count) {
+  //loadList(lists.all[count].viewUrl);
+}
+
+loadList("https://easylist.to/easylist/fanboy-social.txt");
+loadList("https://easylist.to/easylist/fanboy-annoyance.txt");
+loadList("https://easylist.to/easylist/easyprivacy.txt");
+loadList("https://easylist.to/easylist/easylist.txt");
+loadList("https://easylist.to/easylistgermany/easylistgermany.txt");
+loadList("https://easylist-downloads.adblockplus.org/easylistitaly.txt");
+loadList("https://easylist-downloads.adblockplus.org/easylistdutch.txt");
+loadList("https://easylist-downloads.adblockplus.org/liste_fr.txt");
+loadList("https://easylist-downloads.adblockplus.org/easylistchina.txt");
+loadList("http://stanev.org/abp/adblock_bg.txt");
+loadList("https://raw.githubusercontent.com/heradhis/indonesianadblockrules/master/subscriptions/abpindo.txt");
+loadList("https://easylist-downloads.adblockplus.org/Liste_AR.txt");
+loadList("https://raw.githubusercontent.com/tomasko126/easylistczechandslovak/master/filters.txt");
+loadList("https://notabug.org/latvian-list/adblock-latvian/raw/master/lists/latvian-list.txt");
+loadList("https://raw.githubusercontent.com/easylist/EasyListHebrew/master/EasyListHebrew.txt");
+loadList("http://margevicius.lt/easylistlithuania.txt");
+loadList("https://easylist-downloads.adblockplus.org/antiadblockfilters.txt");
+loadList("https://raw.githubusercontent.com/rbrito/easylist-ptbr/master/adblock-rules.txt");
+loadList("http://adb.juvander.net/Finland_adb.txt");
+
+
+// This is a test!
+setTimeout(function () { doFilter("youtube.com", function () { }) }, 1000);
+
+
+var handlers = {};
+handlers.onConnect = logEvent.bind(null, 'onConnect');
+handlers.onDisconnect = logEvent.bind(null, 'onDisconnect');
+handlers.doFilter = doFilter
+handlers.doReplace = doReplace
+const message = msg.init('bg', handlers);
+
+
--- src/js/content.js
+++ src/js/content.js
@@ -2,19 +2,82 @@ import $ from 'jquery';
 import handlers from './modules/handlers';
 import msg from './modules/msg';
 
-// here we use SHARED message handlers, so all the contexts support the same
-// commands. but this is NOT typical messaging system usage, since you usually
-// want each context to handle different commands. for this you don't need
-// handlers factory as used below. simply create individual `handlers` object
-// for each context and pass it to msg.init() call. in case you don't need the
-// context to support any commands, but want the context to cooperate with the
-// rest of the extension via messaging system (you want to know when new
-// instance of given context is created / destroyed, or you want to be able to
-// issue command requests from this context), you may simply omit the
-// `handlers` parameter for good when invoking msg.init()
-
-console.log('CONTENT SCRIPT WORKS!'); // eslint-disable-line no-console
-
-msg.init('ct', handlers.create('ct'));
-
+console.log('Automatic Content filtering...'); // eslint-disable-line no-console
 console.log('jQuery version:', $().jquery); // eslint-disable-line no-console
+
+var message = msg.init('ct', handlers.create('ct'));
+
+function realReplace(sel, idx, html) {
+    $(sel).each(function (index) {
+        if (idx === index) {
+            // Debug
+            //$(this).css({ outline: '0px dashed red' });
+
+
+
+            if ($(this).attr('data-replaced')){
+                $(this).attr('data-replaced', $(this).attr('data-replaced')+1);
+            } else {
+                $(this).attr('data-replaced', 1);
+            }
+            
+            //$(this).css({ outline: '3px dashed green' });
+            $(this).fadeOut("quick", function(){
+                $(this).html(html).fadeIn("quick");
+            });
+
+        }
+    });
+}
+
+
+function replaceElement(element, selector) {
+
+    // Unbind all events from the node and its children
+    element.unbind();
+    element.find('*').unbind();
+
+
+
+    element.each(function (index) {
+
+        var h = $(this).height();
+        var w = $(this).width();
+
+        if (!$(this).attr('data-replaced') || $(this).attr('data-replaced') <= 0) {
+            message.bg('doReplace', selector, index, w, h, function (html) {
+                realReplace(selector, index, html);
+            });
+        }
+
+    });
+
+}
+
+function periodicCheck(selectors) {
+    console.info("Checking selectors..."); // eslint-disable-line no-console
+
+    var elements = selectors.split(',');
+
+    for (var index = 0; index < elements.length; ++index) {
+        if (elements[index] && elements[index].length > 0 && $(elements[index])) {
+            replaceElement($(elements[index]), elements[index]);
+        }
+    }
+
+    setTimeout(function () {
+        periodicCheck(selectors);
+    }, 5000);
+
+}
+
+$(document).ready(function(){
+
+    setTimeout(function () {
+        message.bg('doFilter', window.location.host, function (selectors) {
+            //$(selectors).css({ outline: '3px dashed orange' });
+            periodicCheck(selectors);
+        });
+    }, 500);
+
+});
